6d73595aa9ec22000b872af4f04e4ea643653952
ismail2009 <ismail.1996.1.9@gmail.com>
2020-04-10 22:43:21 +0300

    build formasy input component using formasy

---
+++ src/shared/components/formasy-input/index.js
@@ -0,0 +1,25 @@
+// MyInput.js
+import { withFormsy } from "formsy-react";
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import _ from "lodash";
+// https://github.com/formsy/formsy-react/blob/master/API.md
+// https://material-ui.com/api/text-field/
+
+const MyInput = ({ value, isValid, name, errorMessage, onChange, ...res }) => {
+    // An error message is passed only if the component is invalid
+    const error = _.isEmpty(value) ? false : !isValid;
+    return (
+        <div>
+            <TextField
+                error={error}
+                onChange={onChange(name)}
+                defaultValue={value}
+                helperText={error ? errorMessage : " "}
+                {...res}
+            />
+        </div>
+    );
+};
+
+export default withFormsy(MyInput);
